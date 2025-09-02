57a460e43a1dc288824eb4ccfb04f3ddda7a79c8
unknown <d653@naver.com>
2025-09-02 21:22:59 +0900

    Fix - optimize loaidng speed of image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,13 @@ export default function Home() {
     <div className="flex flex-col h-screen items-center">
       <section className="relative flex flex-col items-center">
         <div className="relative max-w-448 min-w-375 w-full h-752">
-          <Image src={first_cover} alt="첫 번째화 표지" fill />
+          <Image
+            src={first_cover}
+            alt="첫 번째화 표지"
+            fill
+            unoptimized
+            priority
+          />
           <div className="absolute bottom-44 left-60 flex flex-col items-center font-normal text-normal-16 text-nowrap">
             <span>이제 본격적으로</span>
             <span>OO님의 사주팔자를</span>
@@ -20,12 +26,12 @@ export default function Home() {
 
       <section className="relative flex flex-col items-center">
         <div className="relative max-w-448 min-w-351 w-full h-285">
-          <Image src={second_screen} alt="두 번째 화면" fill />
+          <Image src={second_screen} alt="두 번째 화면" fill unoptimized />
         </div>
 
         <div className="absolute max-w-448 min-w-375 w-full">
           <div className="relative top-210 max-w-448 min-w-375 w-full h-410">
-            <Image src={third_screen} alt="세 번째 화면" fill />
+            <Image src={third_screen} alt="세 번째 화면" fill unoptimized />
           </div>
         </div>
 
